05d7a479305d77db8c359c6d7f9c8d72f3c1e06a
DeepakBarwal <deepakbarwal98@gmail.com>
2023-04-08 15:12:46 +0530

    Added addVideo

--- src/controllers/video-controller.js
+++ src/controllers/video-controller.js
@@ -1,8 +1,25 @@
 import { VideoService } from "../services/index.js";
+import { createError } from "../utils/error.js";
 
 const videoService = new VideoService();
 
-export const addVideo = async (req, res, next) => {};
+export const addVideo = async (req, res, next) => {
+  try {
+    const video = await videoService.videoRepository.create({
+      userId: req.user.id,
+      ...req.body,
+    });
+    return res.status(201).json({
+      success: true,
+      message: "Successfully created the video",
+      data: video,
+      err: {},
+    });
+  } catch (error) {
+    console.error(error);
+    next(createError(error, error.status));
+  }
+};
 
 export const updateVideo = async (req, res, next) => {};
 
